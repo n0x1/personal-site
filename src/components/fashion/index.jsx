a509a8fd0cffc85290ba21cefe9ac8042f1af146
cam manzo <71352417+n0x1@users.noreply.github.com>
2025-09-05 17:53:23 -0400

    Update index.jsx

--- src/components/fashion/index.jsx
+++ src/components/fashion/index.jsx
@@ -21,7 +21,7 @@ function Fashion() {
                     textHeader="fashion" 
                     headerColor="inherit" 
                     bodyColor="bg-slate-900" 
-                    bodyText="i like fashion more than 99% of CS ppl /  my favs: earthy tones, zipper hoodies, chunky jewlery. i have kinda a mix of capsule and thrifted statement stuff. i wanna go back to harajuku i would wear more fem stuff if i wasnt afriad of being judged but i like the options. my style now is more andro iwould say and i wear a mix of mens and womens but still baggy stuff and not experimenting. it feels extremely limiting when u dont wanna be out of place yet also show your indiviuality"
+                    bodyText="Tbd revision"
                 />
         </div>
         </>
